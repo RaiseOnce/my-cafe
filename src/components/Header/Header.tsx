cc5996b314a1c6eedce07d0b6c1224e0c32bb270
Otto Rahn <elias.yunusoff@gmail.com>
2025-09-17 16:34:06 +0500

    some fixes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,10 @@ import { Container } from '../Container/Container'
 import { Logo } from '../../assets/Logo'
 import Link from 'next/link'
 import { Globe } from '@/assets/Globe'
+import { Button } from '@/ui/Button/Button'
+import { Loupe } from '@/assets/Loupe'
+import { Location } from '@/assets/Location'
+import { Time } from '@/assets/Time'
 type Props = {}
 
 export default function Header({}: Props) {
@@ -14,17 +18,32 @@ export default function Header({}: Props) {
           {/* <Link className={styles.logo} href="/">
             <Logo />
           </Link> */}
+          <Loupe />
           <input />
-          <div>Address</div>
+
+          <div className={styles.address}>
+            <div className={styles.location}>
+              <Button className={styles.locationBtn}>
+                <Location className={styles.locationSvg} />
+                <span>улица Первая</span>
+              </Button>
+            </div>
+            <div className={styles.time}>
+              <Button className={styles.timeBtn}>
+                <Time className={styles.timeSvg} />
+                <span>Сейчас</span>
+              </Button>
+            </div>
+          </div>
         </div>
 
         <div className={styles.right}>
-          <button className={styles.lang}>
+          <Button className={styles.lang}>
             <Globe />
-          </button>
-          <button className={styles.auth}>
+          </Button>
+          <Button className={styles.auth}>
             <span>Войти</span>
-          </button>
+          </Button>
         </div>
       </Container>
     </header>
--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -1,33 +1,55 @@
 import React from 'react'
 import styles from './Button.module.scss'
 
-type ButtonSize = 'small' | 'medium' | 'large'
-type ButtonVariant = 'primary' | 'secondary' | 'outline'
-
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: ButtonSize
-  variant?: ButtonVariant
+  className?: string
 }
 
-export const Button: React.FC<ButtonProps> = ({
-  size = 'medium',
-  variant = 'primary',
-  className = '',
-  ...props
-}) => {
+export const Button: React.FC<ButtonProps> = ({ className, children }) => {
   // формируем строку классов вручную
   let buttonClass = styles.button
-
-  if (size === 'small') buttonClass += ` ${styles.small}`
-  if (size === 'medium') buttonClass += ` ${styles.medium}`
-  if (size === 'large') buttonClass += ` ${styles.large}`
-
-  if (variant === 'primary') buttonClass += ` ${styles.primary}`
-  if (variant === 'secondary') buttonClass += ` ${styles.secondary}`
-  if (variant === 'outline') buttonClass += ` ${styles.outline}`
 
   // добавляем кастомные классы, если переданы
   if (className) buttonClass += ` ${className}`
 
-  return <button className={buttonClass} {...props} />
+  return <button className={buttonClass}>{children}</button>
 }
+
+// import React from 'react'
+// import styles from './Button.module.scss'
+
+// type ButtonSize = 'small' | 'medium' | 'large'
+// type ButtonVariant = 'primary' | 'secondary' | 'outline'
+
+// interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+//   size?: ButtonSize
+//   variant?: ButtonVariant
+// }
+
+// export const Button: React.FC<ButtonProps> = ({
+//   size = 'medium',
+//   variant = 'primary',
+//   className = '',
+//   children,
+//   ...props
+// }) => {
+//   // формируем строку классов вручную
+//   let buttonClass = styles.button
+
+//   if (size === 'small') buttonClass += ` ${styles.small}`
+//   if (size === 'medium') buttonClass += ` ${styles.medium}`
+//   if (size === 'large') buttonClass += ` ${styles.large}`
+
+//   if (variant === 'primary') buttonClass += ` ${styles.primary}`
+//   if (variant === 'secondary') buttonClass += ` ${styles.secondary}`
+//   if (variant === 'outline') buttonClass += ` ${styles.outline}`
+
+//   // добавляем кастомные классы, если переданы
+//   if (className) buttonClass += ` ${className}`
+
+//   return (
+//     <button className={buttonClass} {...props}>
+//       {children}
+//     </button>
+//   )
+// }
