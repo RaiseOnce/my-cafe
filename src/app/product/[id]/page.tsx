51548a9cbe07604aaa30fa1b8c3a1f322dab3c2d
Otto Rahn <elias.yunusoff@gmail.com>
2026-01-09 23:03:10 +0500

    product page is pre-done

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,6 +2,9 @@ import styles from './page.module.scss'
 import { notFound } from 'next/navigation'
 import { prisma } from '../../../../prisma/prisma-client'
 import Image from 'next/image'
+import { Button } from '@/ui/Button/Button'
+import { Minus } from '@/assets/Minus'
+import { Plus } from '@/assets/Plus'
 
 type Props = {
   params: Promise<{
@@ -34,37 +37,52 @@ export default async function ProductPage({ params }: Props) {
           />
         </div>
         <div className={styles.right}>
-          <h2 className={styles.title}>Название</h2>
-          <div className={styles.weight}>300 г</div>
-          <div className={styles.text}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam optio
-            dolorem praesentium placeat! Et, delectus porro molestiae aut quos
-            exercitationem, tempore vero vel temporibus est repellat veniam
-            maxime saepe perferendis?
+          <div className={styles.top}>
+            <h2 className={styles.title}>Название</h2>
+            <div className={styles.weight}>300 г</div>
+            <h3 className={styles.subtitle}>Описание</h3>
+            <div className={styles.text}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              optio dolorem praesentium placeat! Et, delectus porro molestiae
+              aut quos exercitationem, tempore vero vel temporibus est repellat
+              veniam maxime saepe perferendis?
+            </div>
+            <h3 className={styles.subtitle}>Состав</h3>
+            <div className={styles.text}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              optio dolorem praesentium placeat!
+            </div>
+            <div className={styles.items}>
+              <div className={styles.item}>
+                <div className={styles.itemValue}>19</div>
+                <div className={styles.itemLabel}>Белки</div>
+              </div>
+              <div className={styles.item}>
+                <div className={styles.itemValue}>11</div>
+                <div className={styles.itemLabel}>Жиры</div>
+              </div>
+              <div className={styles.item}>
+                <div className={styles.itemValue}>15</div>
+                <div className={styles.itemLabel}>Углеводы</div>
+              </div>
+              <div className={styles.item}>
+                <div className={styles.itemValue}>214</div>
+                <div className={styles.itemLabel}>Ккал</div>
+              </div>
+            </div>
           </div>
-          <h3 className={styles.subtitle}>Состав</h3>
-          <div className={styles.text}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam optio
-            dolorem praesentium placeat!
-          </div>
-          <h3 className={styles.subtitle}>Пищевая ценность</h3>
-          <div className={styles.items}>
-            <div className={styles.item}>
-              <div className={styles.itemLabel}>Белки</div>
-              <div className={styles.itemValue}>19</div>
-            </div>
-            <div className={styles.item}>
-              <div className={styles.itemLabel}>Жиры</div>
-              <div className={styles.itemValue}>11</div>
-            </div>
-            <div className={styles.item}>
-              <div className={styles.itemLabel}>Углеводы</div>
-              <div className={styles.itemValue}>15</div>
-            </div>
-            <div className={styles.item}>
-              <div className={styles.itemLabel}>Энерг. ценность</div>
-              <div className={styles.itemValue}>214 ккал</div>
-            </div>
+          <div className={styles.bottom}>
+            <div className={styles.price}>400 TMT</div>
+
+            <Button className={styles.btn}>
+              {/* <span className={styles.minus}>
+                <Minus />
+              </span> */}
+              <span>Добавить</span>
+              {/* <span className={styles.plus}>
+                <Plus />
+              </span> */}
+            </Button>
           </div>
         </div>
       </div>
